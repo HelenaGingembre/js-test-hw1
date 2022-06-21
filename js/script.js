f4d7fa8d3894e2d419ef7d52d93aafe35e61bf32
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-06-21 23:59:02 +0300

    task <=25

--- js/script.js
+++ js/script.js
@@ -320,12 +320,13 @@ console.log(checkStorage20(200, 250));
 //----------------------------
 console.log("Задача 21");
 function isNumberInRange(start, end, number) {
-  const isInRange = (start <= number && number <= end); // Change this line
+  const isInRange = (number >= start  && number <= end); // Change this line
   return isInRange;
 } 
 
 
 console.log(isNumberInRange(10, 30, 17));
+console.log(isNumberInRange(10, 30, 50));
 //----------------------------
 console.log("Задача 22");
 /*Функция checkIfCanAccessContent(subType) проверяет,
@@ -360,17 +361,50 @@ console.log(isNumberNotInRange(10, 30, 5));
 
 //----------------------------
 console.log("Задача 24");
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+  // Change code below this line
+if (totalSpent >= 50000){
+  discount = GOLD_DISCOUNT;
+}
+else if( totalSpent >= 20000 && totalSpent <= 50000){
+ discount = SILVER_DISCOUNT;
+}
+else if(totalSpent >= 5000 && totalSpent <= 20000){
+  discount = BRONZE_DISCOUNT;
+}
+else {
+  discount = BASE_DISCOUNT;
+}
+
+  // Change code above this line
+  return discount;
+}
+console.log(getDiscount(8250));
+console.log(getDiscount(46900));
+console.log(getDiscount(137000));
+console.log(getDiscount(1300));
 
 
-console.log(" ");
-console.log(" ");
-console.log(" ");
 //----------------------------
 console.log("Задача 25");
+function checkStorage(available, ordered) {
+  let message;
+  // Change code below this line
+  
+    message = (ordered > available) ? "Not enough goods in stock!" : "The order is accepted, our manager will contact you";
+   // Change code above this line
+  return message;
+}
 
-console.log(" ");
-console.log(" ");
-console.log(" ");
+
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+
 //----------------------------
 console.log("Задача 26");
 
