6eb4722158acc700600eec63cce56d6a8c8504bf
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-06-30 15:54:15 +0300

    update js example

--- js/example_1.js
+++ js/example_1.js
@@ -10,25 +10,24 @@
 реченням 'Правильно!', якщо ні 'Це ECMAScript!'.
 */
 console.log("----------- Example 1 ---------------");
-let question = prompt("Яка офіційна назва в JavaScript?");
-console.log("question", question);
-let message;
-let normalizedInput = question.toLowerCase();
-let trueAnswer = "E'CMAScript";
-if (question) {
-    
-   // message = alert(`введене нічого це - ${typeof question}`); 
-    //message = alert('test != null!');
-    if (normalizedInput === trueAnswer.toLowerCase()) {
-    message = alert('Правильно!');
-    }
-    else {
-    message = alert('Відповідь неправильна!'); 
-    }
-}
-else { //не працює
-   
-     message = alert('Нічого не введено, введіть якусь відповідь!'); 
-    
-}
+function questionJavaScript(){
+     let question = prompt("Яка офіційна назва в JavaScript?");
+     console.log("answer", question);
+     let normalizedInput = question.toLowerCase();
+     let trueAnswer = "ECMAScript";
+     if (question) {
+          //перевірка alert(`введене нічого це - ${typeof question}`); 
+     //message = alert('test != null!');
+     if (normalizedInput === trueAnswer.toLowerCase()) {
+          return alert('Правильно!');
+          console.log('Відповідь правильна!', question );
+     }
+     else {
+          return alert('Відповідь неправильна!'); 
+          console.log('Відповідь неправильна!');
+     }
+     }
 
+     return alert('Нічого не введено, введіть якусь відповідь!'); 
+}
+console.log(questionJavaScript());
--- js/example_2.js
+++ js/example_2.js
@@ -9,3 +9,40 @@ console.log("----------- Example 2 ---------------");
 
 const hours = 14;
 const minutes = 26;
+
+
+
+console.log("----------- Example 2 (поточна дата)---------------");
+function getDate() {
+    let now = new Date(); //
+    
+    let days = ["Неділя", "Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота"];
+    let hours = {//console.log(result);
+        13: 1,
+        14: 2,
+        15: 3,
+        16: 4,
+        17: 5,
+        18: 6,
+        19: 7,
+        20: 8,
+        21: 9,
+        22: 10,
+        23: 11
+    };
+
+    //Метод .getDay() возвращает порядковый номер дня недели
+    //указанной даты по местному времени, где 0 соответствует воскресенью.
+    
+    // .getHours() возвращает часы указанной даты по местному времени.
+    //getMinutes() возвращает минуты указанной даты по местному времени.
+   let  result = 'Сьогодні : ' + days[now.getDay()] + '\nПоточний час: ';
+    result += (now.getHours() >= 12 && now.getHours() <= 23) ?
+        (hours[now.getHours() - 1] + ' PM(годин): ') : now.getHours() + ' AM(годин): ';
+    result += now.getMinutes() + ' хв : ' + now.getSeconds()+ ' с.';
+
+    //alert(result);
+    return result;
+}
+
+console.log(getDate());
