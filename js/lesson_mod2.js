ad941c2d1a97be775bd3b41433fffeb576aa8e68
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-07-03 16:43:17 +0300

    update lesson mod2

--- js/lesson_mod2.js
+++ js/lesson_mod2.js
@@ -14,6 +14,7 @@ console.log(result);
 console.log(result.join(" "));
 
 // Example 6 
+//var 1 ----------------
 function invertedString(stringTest){
 let invertedString = '';
     for (let i = stringTest.length-1; i >= 0; i -= 1) {
@@ -24,7 +25,7 @@ let invertedString = '';
 }
 console.log(invertedString("Welcom to the future"));
 console.log(invertedString("Hello world"));
-
+//var 2 ----------------
 function invertedStringSecond(string2){
     let invertedStringSecond = string2.split('');
     console.log(invertedStringSecond);
@@ -33,6 +34,15 @@ function invertedStringSecond(string2){
 console.log(invertedStringSecond("Welcom to the future"));
 console.log(invertedStringSecond("Hello world"));
 
+//var 3 ----------------
+function reverseNumber() {
+    let x = prompt('Введите число: ', 12345);
+    let result = x.toString().split('').reverse().join('');
+
+    alert(`Число ${x} тепер перевернуте: ${result}`)
+    return `Число ${x} тепер перевернуте: ${result}`;
+}
+console.log(reverseNumber());
 // Example 7
 
 const numbers = [2, 17, 94, 1, 23, 37];
@@ -42,5 +52,68 @@ for (let i = 0; i < numbers.length; i += 1){
         min = numbers[i];
     }
 }
-console.log(min);
+console.log('мінімальне число: ' + min);
 console.log(Math.min(...numbers));
+
+
+// Example 8 Pепета
+function changeCase(string8) {
+    
+    const letters = string8.split('');
+    let invertedString = '';
+   
+    for (const letter of letters) {
+        
+        const isInLowetCase = letter === letter.toLowerCase();
+        
+        invertedString += isInLowetCase
+            ? letter.toUpperCase()
+            : letter.toLowerCase();
+    }
+    
+    return ` строка ${string8} в зворотньому регістрі =  ${invertedString}`;
+}
+
+console.log(changeCase('sdkDTRFc'));
+
+// Example 9 Pепета
+function fn(a, b, c, ...args) {
+    console.log(`${a} ${b} ${c} `);
+    console.log(args);
+    
+}
+fn('hello', 1, 2, 3, 4, 5);
+fn('hi', 1, 2, 3);
+
+
+// Example 10 Pепета
+function sum(...args) {
+    console.log(args);
+    let total = 0;
+
+    for (const arg of args) {
+        total += arg;
+}
+    return total;
+}
+
+console.log(sum(15, 20, 3, 4, 5));
+
+// Example 11 Pепета
+console.log("------Example 11 Pепета--------");
+function filterNumbers(array, ...args) {
+    console.log('array:', array);
+    console.log('args:', args);
+    const uniqueElements = [];
+
+    for (const element of array) {
+        if (args.includes(element)) {
+            uniqueElements.push(element);
+
+            console.log(`${element} есть везде!`);
+        }
+}
+    return uniqueElements;
+}
+
+console.log(filterNumbers([5, 15, 30,3,20],15, 20, 3, 4, 5));
---
+++ js/script_hw3.js
@@ -0,0 +1,162 @@
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+
+// Change code below this line
+const ownerName = apartment.owner.name;
+console.log(ownerName);
+
+const ownerPhone = apartment.owner.phone;
+console.log(ownerPhone);
+
+const ownerEmail = apartment.owner.email;
+console.log(ownerEmail);
+
+const numberOfTags = apartment.tags.length;
+console.log(numberOfTags);
+
+const firstTag = apartment.tags[0];
+console.log(firstTag);
+
+const lastTag = apartment.tags[apartment.tags.length - 1];
+console.log(lastTag);
+// Change code above this line
+
+console.log("Завдання 6");
+// Change code below this line
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.owner.name = "Henry Sibola";
+apartment.tags.push("trusted");
+
+console.log(apartment.price);
+console.log(apartment.rating);
+console.log(apartment.owner.name);
+console.log(apartment.tags);
+
+console.log("Завдання 7");
+console.log("/*-------------------*/");
+// Change code below this line
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = {};
+apartment.location.country="Jamaica";
+apartment.location.city="Kingston";
+
+console.log(apartment);
+
+
+console.log("Завдання 8");
+console.log("/*-------------------*/");
+
+console.log("Завдання 9");
+console.log("/*-------------------*/");
+
+
+console.log("Завдання 10");
+console.log("/*-------------------*/");
+
+
+
+console.log("Завдання 11");
+console.log("/*-------------------*/");
+
+console.log("Завдання 12");
+console.log("/*-------------------*/");
+
+
+console.log("Завдання 13");
+console.log("/*-------------------*/");
+
+
+
+console.log("Завдання 14");
+console.log("/*-------------------*/");
+
+console.log("Завдання 15");
+console.log("/*-------------------*/");
+
+
+console.log("Завдання 16");
+console.log("/*-------------------*/");
+
+console.log("Завдання 17");
+console.log("/*-------------------*/");
+
+console.log("Завдання 18");
+console.log("/*-------------------*/");
+
+console.log("Завдання 19");
+console.log("/*-------------------*/");
+
+
+console.log("Завдання 20");
+console.log("/*-------------------*/");
+
+
+
+console.log("Завдання 21");
+console.log("/*-------------------*/");
+
+console.log("Завдання 22");
+console.log("/*-------------------*/");
+
+
+console.log("Завдання 23");
+console.log("/*-------------------*/");
+
+
+
+console.log("Завдання 24");
+console.log("/*-------------------*/");
+
+console.log("Завдання 25");
+console.log("/*-------------------*/");
+
+console.log("Завдання 26");
+console.log("/*-------------------*/");
+
+console.log("Завдання 27");
+console.log("/*-------------------*/");
+
+console.log("Завдання 28");
+console.log("/*-------------------*/");
+
+console.log("Завдання 29");
+console.log("/*-------------------*/");
+
+
+console.log("Завдання 30");
+console.log("/*-------------------*/");
+
+
+
+console.log("Завдання 31");
+console.log("/*-------------------*/");
+
+console.log("Завдання 32");
+console.log("/*-------------------*/");
+
+
+console.log("Завдання 33");
+console.log("/*-------------------*/");
+
+
+
+console.log("Завдання 34");
+console.log("/*-------------------*/");
+
+console.log("Завдання 35");
+console.log("/*-------------------*/");
+
+console.log("Завдання 36");
+console.log("/*-------------------*/");
